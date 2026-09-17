c5219e4ec2ce74181d2dc33e38df66b95490c334
saloninformationsystem <saloninformationsystem@gmail.com>
2026-09-17 14:52:18 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -7,7 +7,7 @@
 
 // システム設定
 const CONFIG = {
-  GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbyZCAwfkwx_r-5IUWYerFZhD3JahVKb8oH4B1aTMsgoPFC93Ck53Cqm8SshYlVXn5Jg/exec",
+  GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbyypp5cWT3DKusUtpXWlVVScNb1aobMBi1cKnms7owGrZ63HBipz7xQuxDG7FihcZmV/exec",
   STORAGE_FIELDS: ["name", "name_kana", "tel", "email"],
   STORAGE_PREFIX: "sis_",
 
